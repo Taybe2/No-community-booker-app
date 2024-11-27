96e9e36334c49c2b77aabcd28b1ac858c189cf21
Taybe Muharem <taybemuharem@gmail.com>
2024-11-27 18:35:52 +0000

    Add Available Times link to nav

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('time-slots/', views.time_slot_view, name='time_slot_view'),
+    path('time-slots/', views.time_slot_view, name='time_slots'),
 ]
